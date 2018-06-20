3c28fa514173e5261f515d6e7ce0bb3534b304b5
Apoorv Gupta <apoorv020@gmail.com>
2018-06-20 16:20:49 -0700

    Update batch processing code to also output the classifier values.

--- qii.py
+++ qii.py
@@ -13,20 +13,21 @@ import numpy.linalg
 import copy
 
 from ml_util import split_and_train_classifier, split_data, get_arguments, \
-     Dataset, measure_analytics, \
-     plot_series_with_baseline, plot_series
+    Dataset, measure_analytics, \
+    plot_series_with_baseline, plot_series
 
 import qii_lib
+
 
 def __main__():
     args = get_arguments()
     qii_lib.record_counterfactuals = args.record_counterfactuals
 
-    #Read dataset
+    # Read dataset
     dataset = Dataset(args.dataset, sensitive=args.sensitive, target=args.target)
-    #Get column names
-    #f_columns = dataset.num_data.columns
-    #sup_ind = dataset.sup_ind
+    # Get column names
+    # f_columns = dataset.num_data.columns
+    # sup_ind = dataset.sup_ind
 
     ######### Begin Training Classifier ##########
 
@@ -35,7 +36,7 @@ def __main__():
     for classifier in classifiers:
         dat = split_and_train_classifier(classifier, args, split_dataset)
 
-        print ('End Training Classifier: %s' % classifier)
+        print('End Training Classifier: %s' % classifier)
         ######### End Training Classifier ##########
 
         measure_analytics(dataset, dat.cls, dat.x_test, dat.y_test, dat.sens_test)
@@ -58,7 +59,7 @@ def __main__():
 
         t_end = time.time()
 
-        print (t_end - t_start)
+        print(t_end - t_start)
     if args.batch_mode:
         args_filename = 'processed_data/args_%s' % args.output_suffix
         with open(args_filename, 'wb') as pickle_file:
@@ -77,6 +78,7 @@ def eval_discrim(dataset, args, dat):
             'Feature', 'QII on Group Disparity',
             baseline)
 
+
 def eval_average_unary_individual(dataset, args, dat):
     """ Unary QII averaged over all individuals. """
 
@@ -87,6 +89,7 @@ def eval_average_unary_individual(dataset, args, dat):
     if args.show_plot or args.output_pdf:
         plot_series(average_local_inf_series, args,
                     'Feature', 'QII on Outcomes')
+
 
 def eval_unary_individual(dataset, args, dat):
     """ Unary QII. """
@@ -100,6 +103,7 @@ def eval_unary_individual(dataset, args, dat):
         plot_series(average_local_inf_series, args,
                     'Feature', 'QII on Outcomes')
 
+
 def eval_banzhaf(dataset, args, dat):
     """ Banzhaf metric. """
 
@@ -109,6 +113,7 @@ def eval_banzhaf(dataset, args, dat):
     banzhaf_series = pd.Series(banzhaf, index=banzhaf.keys())
     if args.show_plot or args.output_pdf:
         plot_series(banzhaf_series, args, 'Feature', 'QII on Outcomes (Banzhaf)')
+
 
 def eval_shapley(dataset, args, dat):
     """ Shapley metric. """
@@ -122,7 +127,7 @@ def eval_shapley(dataset, args, dat):
     x_individual = dat.scaler.transform(row_individual)
 
     shapley, _ = qii_lib.shapley_influence(dataset, dat.cls, x_individual, dat.x_test)
-    print (shapley)
+    print(shapley)
     shapley_series = pd.Series(shapley, index=shapley.keys())
     if args.show_plot or args.output_pdf:
         plot_series(shapley_series, args, 'Feature', 'QII on Outcomes (Shapley)')
@@ -134,12 +139,13 @@ def eval_shapley_batch(dataset, args, dat):
     learning_samples = args.batch_mode_samples
     shapley_saved = numpy.zeros((learning_samples, len(super_indices)))
     x_samples = numpy.zeros(((learning_samples, dataset.num_data.shape[1])))
+    y_samples = numpy.zeros((learning_samples, 1))
     time_last = time.time()
     for i in range(0, learning_samples):
         if i % 20 == 0:
             time_new = time.time()
-            print ('Index:', i)
-            print ('Time:', time_new - time_last)
+            print('Index:', i)
+            print('Time:', time_new - time_last)
             time_last = time_new
         idx = i * (rowsize // learning_samples)
         row_individual = dataset.num_data.ix[idx].reshape(1, -1)
@@ -151,15 +157,20 @@ def eval_shapley_batch(dataset, args, dat):
         shapley_series = pd.Series(shapley, index=shapley.keys())
         for j in range(0, len(super_indices)):
             shapley_saved[i][j] = shapley_series[super_indices[j]]
+        y_samples[i] = dat.cls.predict(x_individual)
 
     suffix = args.output_suffix
     x_filename = getfilename("processed_data/", "x_samples", suffix)
     qii_filename = getfilename("processed_data/", "qii_samples", suffix)
+    y_filename = getfilename("processed_data/", "y_samples", suffix)
 
     numpy.save(x_filename, x_samples)
     numpy.save(qii_filename, shapley_saved)
+    numpy.save(y_filename, y_samples)
+
 
 def getfilename(prefix, name, suffix):
     return prefix + name + suffix
+
 
 __main__()
